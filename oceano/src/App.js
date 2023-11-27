802812ce982ec115e88b4dc677f94a7680944391
johann <jbois@gaming.tech>
2023-11-27 18:20:26 +0100

    ajout d'une navbar et d'une searchbar

--- oceano/src/App.js
+++ oceano/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Act from "./pages/act.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route 
+} from "react-router-dom"
+
+function App(props){
+  return <Router>
+      <Switch>
+        <Route exact path="/">
+          <Act /> 
+        </Route>
+      </Switch>
+  </Router>
 }
 
 export default App;
