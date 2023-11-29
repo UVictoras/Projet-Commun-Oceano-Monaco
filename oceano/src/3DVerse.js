b826b46b9a9ec9477b16e6557f123cf40504dcb5
johann <jbois@gaming.tech>
2023-11-29 18:46:40 +0100

    debut de fonction

---
+++ oceano/src/3DVerse.js
@@ -0,0 +1,6 @@
+async function Anim(props){
+    // const meshUUID = 'dd582951-6348-4fca-a352-294de65f515c'
+    // const entiy = await SDK3DVerse.engineAPI.findEntitiesByEUID('dd582951-6348-4fca-a352-294de65f515c');
+    console.log("aaa");
+}
+export default Anim
