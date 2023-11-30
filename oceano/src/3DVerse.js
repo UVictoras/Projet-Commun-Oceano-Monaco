89d8aff53f054338ae41f3fb23f4d26f43a506ed
Victoras <vmartinan@gaming.tech>
2023-11-30 13:37:28 +0100

    fixed anim call

--- oceano/src/3DVerse.js
+++ oceano/src/3DVerse.js
@@ -4,7 +4,7 @@ import { Canvas } from "./Canva.jsx";
 async function Anim(props){
 
     const meshUUID = 'b9cf175a-1a94-4247-b9ab-b0cb006e9feb';
-    const entity = await Canvas.SDK3DVerse.engineAPI.findEntitiesByEUID('dd582951-6348-4fca-a352-294de65f515c');
+    const entity = await window.SDK3DVerse.engineAPI.findEntitiesByEUID('dd582951-6348-4fca-a352-294de65f515c');
     console.log(entity);
 }
 export default Anim
