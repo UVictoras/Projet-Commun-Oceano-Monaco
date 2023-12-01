81e7e443ca397d71415fa76ab9ece109d94e9be3
Nattan <nleguyader@gaming.tech>
2023-12-01 18:16:46 +0100

    user functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dbo = require("./db/db");
 const bodyParser = require('body-parser');
+const sha1 = require('js-sha1');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 4444;
@@ -16,10 +17,11 @@ app.listen(port, function () {
   console.log(`App listening on port ${port}!`);
 });
 
-app.get("/pokemon/list", function (req, res) {
+/*------------- GET USER --------------*/
+app.get("/user", function (req, res) {
   const dbConnect = dbo.getDb();
   dbConnect
-          .query("SELECT * FROM User", 
+          .query("SELECT * FROM User WHERE id = " + body.id, 
           
           function (err, result) {
             if (err){
@@ -30,12 +32,46 @@ app.get("/pokemon/list", function (req, res) {
           })
 });
 
-app.post('/pokemon/insert', jsonParser, (req, res) => {
+/*------------- INSERT USER --------------*/
+app.post('/user/insert', jsonParser, (req, res) => {
     const dbConnect = dbo.getDb();
     const body = req.body;
     console.log('Got body:', body);
     dbConnect
-            .query("INSERT INTO `user`(`last_name`, `first_name`, `pseudo`) VALUES (\"" + body.last_name + "\",\"" + body.first_name + "\",\"" + body.pseudo + "\")", 
+            .query("INSERT INTO User (last_name          , \
+                                      first_name         , \
+                                      pseudo             , \
+                                      email              , \
+                                      password           , \
+                                      picture            , \
+                                      x                  , \
+                                      y                  , \
+                                      z                  , \
+                                      type               , \
+                                      level              , \
+                                      money              , \
+                                      xp             , \
+                                      streak_max         , \
+                                      streak_in_progress , \
+                                      banner             , \
+                                      title              ) \
+                    VALUES (\"" + body.last_name      + "\" , \
+                            \"" + body.first_name     + "\" , \
+                            \"" + body.pseudo         + "\" , \
+                            \"" + body.email          + "\" , \
+                            \"" + sha1(body.password) + "\" , \
+                              " + body.picture        + "   , \
+                              " + body.x              + "   , \
+                              " + body.y              + "   , \
+                              " + body.z              + "   , \
+                            \"" + body.type           + "\" , \
+                                    0                       , \
+                                    0                       , \
+                                    0                       , \
+                                    0                       , \
+                                    0                       , \
+                              " + body.banner         + "   , \
+                              " + body.title          + ")", 
             
             function (err, result) {
                 if (err){
@@ -46,12 +82,24 @@ app.post('/pokemon/insert', jsonParser, (req, res) => {
     res.json(body);
 });
 
-app.post('/pokemon/update', jsonParser, (req, res) => {
+/*------------- UPDATE Personal Data USER --------------*/
+app.post('/user/update/personal_data', jsonParser, (req, res) => {
     const dbConnect = dbo.getDb();
     const body = req.body;
     console.log('Got body:', body);
     dbConnect
-            .query("UPDATE `user` SET `last_name`= \"" + body.last_name + "\" WHERE `id`= " + body.id,
+            .query("UPDATE User SET last_name  = \"" + body.last_name      + "\" , \
+                                    first_name = \"" + body.first_name     + "\" , \
+                                    pseudo     = \"" + body.pseudo         + "\" , \
+                                    email      = \"" + body.email          + "\" , \
+                                    password   = \"" + sha1(body.password) + "\" , \
+                                    picture    =   " + body.picture        + "   , \
+                                    x          =   " + body.x              + "   , \
+                                    y          =   " + body.y              + "   , \
+                                    z          =   " + body.z              + "   , \
+                                    banner     =   " + body.banner         + "   , \
+                                    title      =   " + body.title          + "     \
+                    WHERE id = " + body.id,
             function (err, result) {
               if (err){
                 throw err;
@@ -61,17 +109,18 @@ app.post('/pokemon/update', jsonParser, (req, res) => {
     res.json(body);
 });
 
-app.delete('/pokemon/delete', jsonParser, (req, res) => {
-    const dbConnect = dbo.getDb();
-    const body = req.body;
-    console.log('Got body:', body);
-    dbConnect
-            .query("DELETE FROM `user` WHERE `id` = " + body.id,
-            function (err, result) {
-              if (err){
-                throw err;
-              }
-              console.log(result);     
-            })
-    res.json(body);
+/*------------- DELETE USER --------------*/
+app.delete('/user/delete', jsonParser, (req, res) => {
+  const dbConnect = dbo.getDb();
+  const body = req.body;
+  console.log('Got body:', body);
+  dbConnect
+          .query("DELETE FROM User WHERE id = " + body.id,
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            console.log(result);     
+          })
+  res.json(body);
 });
