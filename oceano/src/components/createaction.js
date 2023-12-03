316307e4d8dfccbc6cee2ea4883e6f71ec9d5ae8
Lilian501 <llafin@gaming.tech>
2023-12-03 10:29:55 +0100

    form create

--- oceano/src/components/createaction.js
+++ oceano/src/components/createaction.js
@@ -1,6 +1,6 @@
 function Create(){
     return(
-        <div>
+        <div class="">
             <p class=" grid place-content-center h-48 ...  text font-medium   text-gray-900 dark:text-white">Choisissez une action à créer</p>
             <form class="max-w-sm mx-auto">
                 <fieldset>
@@ -17,6 +17,12 @@ function Create(){
                             <input id="country-option-1" type="radio" name="countries" value="USA" class="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" checked></input>
                             <label for="country-option-1" class="block ms-2  text-sm font-medium text-gray-900 dark:text-gray-300">
                             Nettoyage de plage
+                            </label>
+                        </div>
+                        <div>
+                            <input id="country-option-1" type="radio" name="countries" value="USA" class="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" checked></input>
+                            <label for="country-option-1" class="block ms-2  text-sm font-medium text-gray-900 dark:text-gray-300">
+                            Un autre truc
                             </label>
                         </div>
                         
@@ -41,6 +47,10 @@ function Create(){
                         defaultValue={''}
                         />
                     </div>
+                </div>
+                <div class="mb-5">
+                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">A quel date se déroulera votre évenement</label>
+                    <input type="text" class ="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Entrer le nom" name="name" required></input>
                 </div>
                 <div class="mb-5">
                     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Créer</button>
