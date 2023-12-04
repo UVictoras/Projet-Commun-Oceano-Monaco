759cd28d42e408487fce95972ec2b1335a07ac33
loanalix <lalix@gaming.tech>
2023-12-04 16:22:26 +0100

    truc

--- index.js
+++ index.js
@@ -124,3 +124,265 @@ app.delete('/user/delete', jsonParser, (req, res) => {
           })
   res.json(body);
 });
+
+
+
+/*------------- GET Banner --------------*/
+app.get("/banner", function (req, res) {
+  const dbConnect = dbo.getDb();
+  dbConnect
+          .query("SELECT * FROM Banner" , 
+          
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            res.json(result);
+            console.log(result);     
+          })
+});
+
+/*------------- GET Animal  --------------*/
+app.get("/animal", function (req, res) {
+  const dbConnect = dbo.getDb();
+  dbConnect
+          .query("SELECT * FROM Animal ", 
+          
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            res.json(result);
+            console.log(result);     
+          })
+});
+
+/*------------- GET Accessories  --------------*/
+app.get("/accessories", function (req, res) {
+  const dbConnect = dbo.getDb();
+  dbConnect
+          .query("SELECT * FROM Accessories", 
+          
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            res.json(result);
+            console.log(result);     
+          })
+});
+
+/*------------- GET Emote  --------------*/
+app.get("/emote", function (req, res) {
+  const dbConnect = dbo.getDb();
+  dbConnect
+          .query("SELECT * FROM Emote ", 
+          
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            res.json(result);
+            console.log(result);     
+          })
+});
+
+/*------------- GET Region  --------------*/
+app.get("/region", function (req, res) {
+  const dbConnect = dbo.getDb();
+  dbConnect
+          .query("SELECT * FROM Region" , 
+          
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            res.json(result);
+            console.log(result);     
+          })
+});
+
+/*------------- UPDATE Region --------------*/
+app.post('/user/update', jsonParser, (req, res) => {
+  const dbConnect = dbo.getDb();
+  const body = req.body;
+  console.log('Got body:', body);
+  dbConnect
+          .query("UPDATE User SET Rate  = \"" + body.Rate      + "\" , \
+                                Rate      =   " + body.title          + "     \
+                  WHERE id = " + body.id,
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            console.log(result);     
+          })
+  res.json(body);
+});
+
+
+/*------------- GET Badges  --------------*/
+app.get("/badges", function (req, res) {
+  const dbConnect = dbo.getDb();
+  dbConnect
+          .query("SELECT * FROM Badges" , 
+          
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            res.json(result);
+            console.log(result);     
+          })
+});
+
+
+/*------------- GET Event  --------------*/
+app.get("/event", function (req, res) {
+  const dbConnect = dbo.getDb();
+  dbConnect
+          .query("SELECT * FROM Event WHERE id = " + body.id, 
+          
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            res.json(result);
+            console.log(result);     
+          })
+});
+
+// /*------------- UPDATE Personal Data Event --------------*/
+// app.post('/event/update/personal_data', jsonParser, (req, res) => {
+//   const dbConnect = dbo.getDb();
+//   const body = req.body;
+//   console.log('Got body:', body);
+//   dbConnect
+//           .query("UPDATE User SET title       = \""         + body.title          + "\" , \
+//                                   description = \""     + body.description    + "\" , \
+//                                   start_date  = \""     + body.start_date     + "\" , \
+//                                   end_date    = \""     + body.end_date       + "\" , \
+//                                   type        = \""     + body.type           + "\" , \
+//                                   image       =   "     + body.image          + "   , \
+//                                   x           =   "     + body.x              + "   , \
+//                                   y           =   "     + body.y              + "   , \
+//                                   z           =   "     + body.z              + "   , \
+//                                   region      =   "     + body.region         + "   , \
+//                                   title       =   "     + body.title          + "     \
+//                   WHERE id = " + body.id,
+//           function (err, result) {
+//             if (err){
+//               throw err;
+//             }
+//             console.log(result);     
+//           })
+//   res.json(body);
+// });
+
+/*------------- DELETE Event --------------*/
+app.delete('/event/delete', jsonParser, (req, res) => {
+  const dbConnect = dbo.getDb();
+  const body = req.body;
+  console.log('Got body:', body);
+  dbConnect
+          .query("DELETE FROM Event WHERE id = " + body.id,
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            console.log(result);     
+          })
+  res.json(body);
+});
+
+
+/*------------- GET thread  --------------*/
+app.get("/thread", function (req, res) {
+  const dbConnect = dbo.getDb();
+  dbConnect
+          .query("SELECT * FROM thread WHERE id = " + body.id, 
+          
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            res.json(result);
+            console.log(result);     
+          })
+});
+
+
+/*------------- GET message  --------------*/
+app.get("/messaage", function (req, res) {
+  const dbConnect = dbo.getDb();
+  dbConnect
+          .query("SELECT * FROM message WHERE id = " + body.id, 
+          
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            res.json(result);
+            console.log(result);     
+          })
+});
+
+/*------------- UPDATE Personal Data message --------------*/
+app.post('/message/update/personal_data', jsonParser, (req, res) => {
+  const dbConnect = dbo.getDb();
+  const body = req.body;
+  console.log('Got body:', body);
+  dbConnect
+          .query("UPDATE User SET last_name  = \"" + body.last_name      + "\" , \
+                                  first_name = \"" + body.first_name     + "\" , \
+                                  pseudo     = \"" + body.pseudo         + "\" , \
+                                  email      = \"" + body.email          + "\" , \
+                                  password   = \"" + sha1(body.password) + "\" , \
+                                  picture    =   " + body.picture        + "   , \
+                                  x          =   " + body.x              + "   , \
+                                  y          =   " + body.y              + "   , \
+                                  z          =   " + body.z              + "   , \
+                                  banner     =   " + body.banner         + "   , \
+                                  title      =   " + body.title          + "     \
+                  WHERE id = " + body.id,
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            console.log(result);     
+          })
+  res.json(body);
+});
+
+/*------------- DELETE message --------------*/
+app.delete('/message/delete', jsonParser, (req, res) => {
+  const dbConnect = dbo.getDb();
+  const body = req.body;
+  console.log('Got body:', body);
+  dbConnect
+          .query("DELETE FROM message WHERE id = " + body.id,
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            console.log(result);     
+          })
+  res.json(body);
+});
+
+
+
+/*------------- GET level  --------------*/
+app.get("/level", function (req, res) {
+  const dbConnect = dbo.getDb();
+  dbConnect
+          .query("SELECT * FROM Level" , 
+          
+          function (err, result) {
+            if (err){
+              throw err;
+            }
+            res.json(result);
+            console.log(result);     
+          })
+});
