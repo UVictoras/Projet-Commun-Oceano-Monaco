3cf8cf7bcd0aefda204082980657eb80f073f39f
johann <jbois@gaming.tech>
2023-12-12 01:12:15 +0100

    pas fait grand chose

--- oceano/src/components/above.js
+++ oceano/src/components/above.js
@@ -2,11 +2,17 @@ import { Click, OpenModal } from "../utils/3DVerse";
 import Modal from "./modal";
 import Searchbar from "./searchbar";
 
-function Above(props){
+function Above(props) {
 
     return <div className="Act absolute">
-        <Searchbar />
-        w
+        <div className="flex items-center">
+            <button className="w-[70px] h-[73px] rounded-2xl border-2 border-solid border-neutral-200 bg-neutral-50 flex items-center bellButton">
+                <img src="img/icon/bell.png" />
+            </button>
+            <Searchbar />
+
+        </div>
+
         {/* <Modal isVisible={() =>Click()} />
          */}
     </div>
