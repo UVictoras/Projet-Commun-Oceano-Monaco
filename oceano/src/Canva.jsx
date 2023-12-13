b7a2586acf37fbef95f6abfd276e6149c48e1ac8
johann <jbois@gaming.tech>
2023-12-14 00:39:21 +0100

    modal  event plus ou moin finit

--- oceano/src/Canva.jsx
+++ oceano/src/Canva.jsx
@@ -71,7 +71,7 @@ export const Canvas = (props) => {
 
         }
 
-    }, [status, label, dom]);
+    }, [status]);
     // ,splineDisplay, three
     
     return <>
--- oceano/src/components/tabsEvent.js
+++ oceano/src/components/tabsEvent.js
@@ -8,7 +8,7 @@ export default function Tab() {
             content: "Nous demandons au gouvernement islandais d'interdire la chasse à la baleine une fois pour toutes. Selon un rapport de l'autorité alimentaire et vétérinaire islandaise, les baleines ont mis jusqu'à deux heures pour mourir lors des chasses islandaises. J’ai créé cet évènement dans l’objectif de sauver tout ces anima... ",
             span: "Voir plus",
             imgDatePicker: "img/event/calendar.png",
-            textDatePicker: "Du 14 Mai au 8 Juin",
+            textDatePicker: "Du   au 8 Juin",
             imgLocate: "img/event/locate.png",
             textLocate: "En ligne",
             imgLink: "img/event/link.png",
@@ -26,8 +26,18 @@ export default function Tab() {
             content: "Hello je m’appelle Loris P., j’ai 19 ans et depuis toujours je kiffe les poissons, je les préfère en sauce LOL, non en vrai je suis passionné des océans et membre de beaucoup d’associations dans ma région, si jamais vous aussi vous êtes passionné n’hésitez pas à me contacter pour discuter...",
             span: "Voir plus",
             color: "fontColor3C",
+            imgProfile: "img/victor.jpg",
+            name: "Victor M.",
+            titleLevel: "Héros des Grecs",
+            level: "Niveau 4",
             participer: "Participer",
             logoShare: "img/event/share.png",
+            linkImage: "",
+            linkText: "Oceanic Conservation",
+            phoneImage: "img/event/phone.png",
+            phoneText: "06.26.89.27.95",
+            mailImage:"img/event/mail.png",
+            mailText:"lorisrpauwels@gmail.com",
         }
     ];
 
@@ -56,31 +66,72 @@ export default function Tab() {
                         key={item.id}
                         className={`${item.color} panel flex  ${checkActive(item.id, "active flex nunito400")}`}
                     >
+                        {item.id === 2 ? <div className="grid grid-cols-2">
+
+                            <div className="profile ">
+                                <div className="grid grid-cols-2">
+                                    <div className="flex justify-center">
+                                        <img src={item.imgProfile} alt="profile picture Make it blue" className="w-28 h-28 rounded-full" />
+                                    </div>
+                                    <div className="mt-4">
+                                        <p className="blackNunito text-start">{item.name}</p>
+                                        <p className="semiBoldNunito text-start mt-1.5">{item.titleLevel}</p>
+                                        <p className="extraBold800 text-start mt-6">{item.level}</p>
+                                    </div>
+                                </div>
+                                <div className="icon mt-5 flex space-x-4 mb-[22.5px]">
+                                    <div className="w-11 h-11 backD9 rounded-2xl"></div>
+                                    <div className="w-11 h-11 backD9 rounded-2xl"></div>
+                                    <div className="w-11 h-11 backD9 rounded-2xl"></div>
+                                    <div className="w-11 h-11 backD9 rounded-2xl"></div>
+                                </div>
+                            </div>
+                            <div className="contact space-y-5 mt-4">
+                                <div className="flex items-center space-x-4">
+                                    <img src={item.linkImage}/>
+                                    <p>{item.linkText}</p>
+                                </div>
+                                <div className="flex items-center  space-x-4">
+                                    <img src={item.phoneImage}/>
+                                    <p>{item.phoneText}</p>
+                                </div>
+                                <div className="flex items-center  space-x-4">
+                                    <img src={item.mailImage}/>
+                                    <p>{item.mailText}</p>
+                                </div>
+                            </div>
+
+
+                        </div> : ""
+                        }
                         <p className="text-left">{item.content}<span className="blueTextColor">
                             {item.span}
                         </span></p>
 
                         {item.id === 1 ? <div className="mt-7 text-left space-y-6">
-                            <div className="flex">
+                            <div className="flex semiBoldNunito">
                                 <img src={item.imgDatePicker} alt="a" className="w-8" />
                                 <p>{item.textDatePicker}</p>
                             </div>
-                            <div className="flex">
+                            <div className="flex semiBoldNunito">
                                 <img src={item.imgLocate} alt="a" />
                                 <p>{item.textLocate}</p>
                             </div>
-                            <div className="flex">
+                            <div className="flex semiBoldNunito">
                                 <img src={item.imgLink} />
-                                <a href className="underline">{item.textLink}</a>
+                                <a href className="underline blueTextColor">{item.textLink}</a>
                             </div>
-                            <div className="flex">
+                            <div className="flex extraBold800">
                                 <img src={item.imgGoal} />
                                 <p >{item.textGoal}</p>
                             </div>
                             <div className="w-[425px] h-[14px] bg-neutral-200 rounded-l-lg">
                                 <div className=" w-[321px] h-[14px] progressBarObjectif rounded-l-lg"></div>
                             </div>
-                        </div> : ""
+                        </div> :
+                        <div className="lastAction text-start blackNunito text-2xl mt-7">
+                            <h2>Dernières actions</h2>
+                        </div>
                         }
                         <div className="flex mt-[43px]">
                             <boutton className="w-[460px] h-11 blueButton rounded-2xl blackNunito text-white flex items-center justify-center ">{item.participer}</boutton>
