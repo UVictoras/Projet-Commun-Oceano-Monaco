c3b4ed1f598d0bb104ee369c3d13b8fe3aa2802a
jbois <jbois@gaming.tech>
2023-12-20 19:51:28 +0100

    shop a peu pres finit

--- oceano/src/pages/shop.js
+++ oceano/src/pages/shop.js
@@ -1,12 +1,43 @@
 import Navbar from "../components/navbar";
+import ShopItems from "../components/shopItems";
+import TabCollectShop from "../components/tabCollectShop";
+import TabShop from "../components/tabShop";
 
 export default function Shop() {
-    return <>
-        <Navbar />
-        <div className="wrapper flex h-screen w-full">
-            <div className="fixed w-1/3 p-4 bg-gray-200"></div>
-            <div className="w-2/3 scrollable overflow-y-auto p-4"></div>
-        </div>
+    return (
+        <>
+            <Navbar />
 
-    </>
+            <div className="flex mt-4 w-full h-full">
+                <div className="w-1/2">
+                    <div className="h-2/3 ">
+                        <div className="h-3/4 flex items-center justify-center ">
+                            <div className="images">
+                                <img src="img/avatar1.png" className="w-96" />
+                            </div>
+                        </div>
+                        <div className=" flex justify-end mr-11">
+                            <button className="items-end jusitfy-end shopButton px-5 py-2 blackNunito text-white uppercaseText"> Sauvegarder</button>
+                        </div>
+                        <div className="mt-10">
+                            <p className="text-center relative lineShop colorAE extraBold800 text-xl">Ma collection</p>
+                        </div>
+                    </div>
+                    <div className="h-1/3">
+                        <TabCollectShop />
+                    </div>
+                </div>
+                <div className="w-1/2 flex justify-end ">
+                    <div className=" border-l-2 border-neutral-200 ">
+                        <TabShop />
+                    </div>
+
+
+                </div>
+
+
+
+            </div>
+        </>
+    );
 }
