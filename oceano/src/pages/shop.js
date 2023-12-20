1d3771d7a003f078a0f5432ee7908f3ff13cf8f6
loanalix <lalix@gaming.tech>
2023-12-20 14:24:48 +0100

    push

--- oceano/src/pages/shop.js
+++ oceano/src/pages/shop.js
@@ -7,7 +7,7 @@ export default function Shop() {
       <Navbar />
 
       <div className="flex mt-4 justify-between">
-        <div className="w-1/3 pr-5">
+        <div className="w-1/2">
           <div className="image-center flex items-center justify-center h-75%">
             <img src="img/items.svg" alt="Objet à acheter" className="w-[35%]" />
           </div>
@@ -20,9 +20,9 @@ export default function Shop() {
           </div>
           <div className="mb-4 mx-auto">
             <div className="flex items-center">
-              <div className="border-t border-gray-600 w-1/2 "></div>
+              <div className="border-t border-gray-600 w-1/3 "></div>
               <p className="text-center  relative extraBold800 flex-1">Ma collection</p>
-              <div className="border-t border-gray-600 w-1/2 "></div>
+              <div className="border-t border-gray-600 w-1/3 "></div>
             </div>
           </div>
           <div className="w-1/3 w-64  flex items-center  space-x-5 ml-3 mr-3 ">
