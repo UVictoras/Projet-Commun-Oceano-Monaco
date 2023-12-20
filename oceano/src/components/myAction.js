a24aebba272d5da976620cbfd4c151ca48794e83
Lilian <lilian.lafin@gmail.com>
2023-12-20 19:03:27 +0100

    action marche pas

--- oceano/src/components/myAction.js
+++ oceano/src/components/myAction.js
@@ -11,16 +11,25 @@ export default function MyAction(props) {
     },[props.type]);
 
     return <div className=" ">
-        <div className="myActionGreyColor flex items-center rounded-2xl">
+        <div className="myActionGreyColor rounded-2xl">
             <div id={"progressBarEvent" + props.type.Name} className="myActionYellowColor rounded-2xl h-11 flex items-center">
-                <img src={props.type.Logo} className="h-3.5 mx-3.5 " alt="coin make it blue"/>
-                <div className="w-2/3">
-                    <p className="text-white w-2/3 extraBoldNunito text-xl whitespace-nowrap">{props.type.Name}</p>
+                <div>
+                    <img src={props.type.Logo} className="h-3.5 mx-3.5 " alt="coin make it blue"/>
                 </div>
-                <div className="flex justify-end w-1/3 px-3">
-                    <p className="text-white extraBoldNunito text-xl">{props.type.NbEvents}</p>
-                </div>
+                <div class =" flex items-center space-x-80">
+                    <div className="">                    
+                        <p className="text-white w-2/3 extraBoldNunito text-xl whitespace-nowrap">{props.type.Name}</p>                  
+                    </div>
+                    <div className="">
+                        
+                        <p className="text-white  extraBoldNunito text-xl">{props.type.NbEvents}</p>
+                    
+                    </div>
+                    
+                </div>               
+                
             </div>
+             
         </div>
     </div>
 }
