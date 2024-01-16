535b5e2351430cc4af33d4d0d9d9ff940b9b322d
johann <jbois@gaming.tech>
2024-01-16 18:12:25 +0100

    debut de la page impact et réorganisation

--- oceano/src/pages/impact.js
+++ oceano/src/pages/impact.js
@@ -1,29 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
+import EventLike from "../components/eventLike";
+import { getFavoriteEvent, getTypeEvent } from "../api/event";
 
-import AboveImpact from "../components/aboveImpact";
-import LoadingScreen from "./loadingScreen";
-import { SceneImpact } from "../sceneImpact";
+function Impact(props) {
+    const [favoriteEvent, setfavoriteEvent] = useState([]);
+    const [EventShow, setEventShow] = useState([]);
+    const [typeEvent, settypeEvent] = useState([]);
+    const [triTypes, settriTypes] = useState("all");
 
-function Impact(props){
-    const [ load, setLoad ] = useState(false);
+    useEffect(() => {
+        const favoriteEventFetched = getFavoriteEvent({ id: 1 });
+        favoriteEventFetched
+            .then(result => setfavoriteEvent(result))
+            .catch(error => console.error("Error :", error.message))
 
-    const handleCanvaChange = (isLoaded) => {
-        setLoad(isLoaded);
-    };
-    
-    return <div className="impact">
-        {!load ? 
-            (
-                <LoadingScreen/>
-            ) : (
-                null
-            )
+        const typeEventFetched = getTypeEvent();
+        typeEventFetched
+            .then(result => settypeEvent(result))
+            .catch(error => console.error("Error :", error.message))
+    }, [])
+
+    useEffect(() => {
+        if (triTypes === "all") {
+            setEventShow(favoriteEvent)
+        } else {
+            setEventShow(favoriteEvent.filter(fav => fav.Name === triTypes))
         }
-        <Navbar/>
-        
-        <AboveImpact/>
-        <SceneImpact onChange={handleCanvaChange}/>
+    }, [triTypes, favoriteEvent])
+
+    const handleClick = (typeName) => {
+        if (typeName === triTypes) {
+            settriTypes("all")
+        } else {
+            settriTypes(typeName)
+        }
+    };
+    return <div className="impact">
+        <Navbar />
+        <div className="w-full">
+            <div className="w-1/3">
+                <div className='filter '>
+                    <div className='filter overflow-x-scroll horizontalScrollbar  whitespace-nowrap '>
+                        <div className='min-w-full mb-7 my-3.5 space-x-2.5 flex'>
+                            {typeEvent.map((type) => {
+                                return <button onClick={() => handleClick(type.Name)} className='p-1 bg-white border-2 border-neutral-200 rounded-xl flex items-center justify-center '>
+                                    <img src={type.Logo} alt='petition make it blue' className='w-4 ml-2' />
+                                    <p className='text-sm extraBoldNunito px-3'>{type.Name}</p>
+                                </button>
+                            })}
+                        </div>
+                    </div>
+                </div>
+                <div className='space-y-4 h-[700px] customScrollbar overflow-y-scroll'>
+                    {EventShow.map((fav) => {
+                        return <EventLike event={fav} />
+                    })}
+                </div>
+            </div>
+        </div>
 
     </div>
 }
