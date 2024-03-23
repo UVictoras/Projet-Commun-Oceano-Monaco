b46f1531e8ef105345ea91162366dfa60796beaf
johann <jbois@gaming.tech>
2024-03-23 13:54:24 +0100

    modifs de matéo ajout d'image et d'event et de notifs

--- oceano/src/components/bannierFriend.js
+++ oceano/src/components/bannierFriend.js
@@ -1,6 +1,6 @@
 export default function BannierFriend(props) {
 
-    return (<button className="border-2 border-neutral-200 rounded-lg w-full p-2 flex items-center relative mt-3">
+    return <button className="border-2 border-neutral-200 rounded-lg w-full p-2 flex items-center relative mt-3">
         <div className="w-1/5">
             <img src={props.user.Picture} alt="photo de profil Make A Wave" className="w-16" />
         </div>
@@ -10,7 +10,7 @@ export default function BannierFriend(props) {
         </div>
         <div className="badge flex items-center space-x-4 ml-4">
             <div className="h-9 w-9 rounded-xl badgeColor">
-
+                {/* <img src={}/> */}
             </div>
             <div className="h-9 w-9 rounded-xl badgeColor">
 
@@ -25,7 +25,7 @@ export default function BannierFriend(props) {
         <div className="absolute backgroundE45757 top-[-14px] right-4 rounded-md">
             <p className="px-2 py-1 blackNunito text-xs text-white">3ème semaine</p>
         </div>
-    </button>)
+    </button>
 
 
 }
--- oceano/src/components/modalNotif.js
+++ oceano/src/components/modalNotif.js
@@ -18,14 +18,38 @@ export default function ModalNotif(props) {
                 <div className="pb-4 w-full mx-auto">
                     <p className="text-center loune relative extraBoldNunito ">Aujourd’hui</p>
                 </div>
-                <News />
-                <News />
-                <News />
+                <div className="rounded-xl bg-white border-2 border-solid border-neutral-200 rounded-md p-4 mb-4 flex items-center justify-between space-x-1">
+                    <div>
+                        <h1 className="extraBoldNunito fontColor3C text-base">A retenir de la COP28</h1>
+                        <p className="nunito400 text-sm">A la COP28, le projet de texte définitif écarte l’abandon à terme des énergies fossiles.</p>
+                    </div>
+                    <div className="col-start-5">
+                        <img src="img/notif/cop28.svg" alt="COP28 Logo" className="mr-5 w-32 h-32" />
+                    </div>
+                </div>
+                <div className="rounded-xl bg-white border-2 border-solid border-neutral-200 rounded-md p-4 mb-4 flex items-center justify-between space-x-1">
+                    <div>
+                        <h1 className="extraBoldNunito fontColor3C text-base">@philippe veut devenir votre ami !</h1>
+                        <p className="nunito400 text-sm">Vous avez une nouvelle demande d'ami, consultez vos demandes sur votre profil.</p>
+                    </div>
+                    <div className="col-start-5">
+                        <img src="img/notif/girl.svg" alt="COP28 Logo" className="mr-5 w-32 h-32" />
+                    </div>
+                </div>
+                {/* <News />
+                <News /> */}
                 <div className="mb-4 w-full mx-auto">
                     <p className="text-center loune relative extraBoldNunito ">Hier</p>
                 </div>
-                <News />
-                <News />
+                <div className="rounded-xl bg-white border-2 border-solid border-neutral-200 rounded-md p-4 mb-4 flex items-center justify-between space-x-1">
+                    <div>
+                        <h1 className="extraBoldNunito fontColor3C text-base">Un traité historique sur les océans</h1>
+                        <p className="nunito400 text-sm">Vous avez une nouvelle demande d'ami, consultez vos demandes sur votre profil.</p>
+                    </div>
+                    <div className="col-start-5">
+                        <img src="img/notif/traite.svg" alt="COP28 Logo" className="mr-5 w-32 h-32" />
+                    </div>
+                </div>
             </div>
 
         </div>
