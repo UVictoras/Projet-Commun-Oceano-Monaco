8160b067bf63e7c162bba527264feb03de41a1e9
Nattan <nleguyader@gaming.tech>
2024-03-25 00:28:56 +0100

    chat

--- oceano/src/components/receiveMessage.js
+++ oceano/src/components/receiveMessage.js
@@ -2,7 +2,7 @@ export default function ReceiveMessage(props) {
     return <div>
         <p className="nunito500 text-sm greyText">Oscar D.</p>
         <div className="flex items-center space-x-3">
-            <img src="img/profil/loutre.png" alt="photo de profil Make A Wave" className="w-12 h-12" />
+            <img src="img/shop/loutre_all.png" alt="photo de profil Make A Wave" className="w-12 h-12" />
             <div className="backGrey py-3 px-4 rounded-2xl w-auto h-auto">
                 <p className="fontColor3C text-base semiBoldNunito">On se remet ça sur la plage lundi pro ?</p>
             </div>
